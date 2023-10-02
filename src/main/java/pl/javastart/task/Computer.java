8d797ceb6dbde6b7757c6a8c3c4eed75f5180d3e
Mateusz Rojek <mateuszr2047@gmail.com>
2023-10-02 14:25:14 +0200

    Commit 2

--- src/main/java/pl/javastart/task/Computer.java
+++ src/main/java/pl/javastart/task/Computer.java
@@ -10,6 +10,13 @@ public class Computer {
     public Computer() {
     }
 
+    public Computer(String producer, String model, int year, double price) {
+        this.producer = producer;
+        this.model = model;
+        this.year = year;
+        this.price = price;
+    }
+
     void printInfo() {
 
         System.out.println("Producent: " + producer + ", Model: " + model + ", Rok produkcji: " + year + ", Cena: " + price + "zł");
--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -4,18 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Computer computer1 = new Computer();
-        Computer computer2 = new Computer();
-
-        computer1.producer = "Lenovo";
-        computer1.model = "IdeaPad 3";
-        computer1.year = 2021;
-        computer1.price = 2990.9;
-
-        computer2.producer = "Dell";
-        computer2.model = "Latitude 3540";
-        computer2.year = 2019;
-        computer2.price = 2879.4;
+        Computer computer1 = new Computer("Lenovo", "IdeaPad 3", 2021, 2990.9);
+        Computer computer2 = new Computer("Dell", "Latitude 3540", 2019, 2879.4);
 
         computer1.printInfo();
         computer2.printInfo();
